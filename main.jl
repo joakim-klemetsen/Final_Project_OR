fcde8e94a098b6cf6b0af8c91ab4c36e609559c8
joakim-klemetsen <146715963+joakim-klemetsen@users.noreply.github.com>
2024-11-15 16:50:58 +0100

    Added partial solution
    I noticed an error with the 1. constraint. Must update the lfs of the constraint. Currently not part of the market_balance constraint.

--- main.jl
+++ main.jl
@@ -2,9 +2,7 @@
 using CSV, DataFrames, JuMP, HiGHS
 
 # loading data
-data = CSV.read("data/ProjectData.csv", DataFrame)
-
-
+data = CSV.read("data/ModifiedProjectData.csv", DataFrame)
 
 # initilize model
 m = Model(HiGHS.Optimizer)
@@ -13,10 +11,17 @@ m = Model(HiGHS.Optimizer)
 bids = 1:nrow(data)
 parent_bids = unique(data.ParentBidID)
 locations = unique(data.Location)
-pipelines = [(l, k) for l in locations, k in locations if l != k]
+location_combinations = [(l, k) for l in locations, k in locations if l != k]
+periods = unique(data.Period)
 
 # parameters
 cap = 300
+M = Dict()
+for i in parent_bids 
+   M[i] = nrow(data[data.ParentBidID .== i,:])
+end
+
+nrow(data[data.ParentBidID .== 1,:])
 
 # variables
 @variable(m, 0 <= x[i in bids] <= 1)
@@ -26,4 +31,44 @@ cap = 300
 
 # objective
 @objective(m, Max, sum(data[i,"Quantity"]*data[i,"Price"]*x[i] for i in bids) - sum(data[j,"FC"]*z[j] for j in parent_bids))
+
+# constraints
+
+## - 1. market balance ----
+market_balance = Dict() 
+for t in periods
+    for l in locations
+        filtered_data = data[data.Period .== t .& data.Location .== l, :]
+        market_balance[l] = @constraint(m,
+            sum(row.Quantity * x[row.BidID] for row in eachrow(filtered_data)) ==
+            sum(f[(k, l)] for k in locations if (k, l) in location_combinations) - 
+            sum(f[(l, k)] for k in locations if (l, k) in location_combinations)
+        )
+    end
+end
+
+## - 2. minimum acceptance ratio fulfillment ----
+
+### link between acceptance ratio and binary variable
+for i in bids 
+    @constraint(m, x[i] <= y[i])
+end
+
+### ensures that acceptance ratio atleast meets the min. acc. requirement
+for i in bids
+    @constraint(m, x[i] >= data[i,"AR"]*y[i])    
+end
+
+## - 3. fixed cost link ----
+
+### upper bound: becomes active when \sum(y_i) > 0 and forces z_l = 1
+for l in locations
+    @constraint(m, sum(y[i] for i in bids) <= M[l]*z[l])
+end
+
+### lower bound: becomes active when \sum(y_i) = 0 and forces z_l = 0
+for l in locations
+    @constraint(m, sum(y[i] for i in bids) >= z[l])
+end
+
 
